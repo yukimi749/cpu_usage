1a9feb74adfa073dab626020b99d9d1d6a1238ed
Yukimi Miyahara <yukimi_pooh@au.com>
2025-01-01 20:31:24 +0900

    change a node

--- cpu_usage/cpu_utilization.py
+++ cpu_usage/cpu_utilization.py
@@ -26,34 +26,3 @@ def main():
     rclpy.init()
     node = Cpu_utilization()
     rclpy.spin(node)
-        
-#rclpy.init()
-#node = Node("cpu_utilization")
-#pub = node.create_publisher(Float32, "cpu", 10)
-
-#def cb():
-    #msg = Float32()
-    #result = subprocess.run(["/usr/bin/top", "-b", "-n", "1"], stdout=subprocess.PIPE, text=True)
-    #output = result.stdout.splitlines()
-
-    #if len(output) >= 3:
-        #cpu_line = output[2]
-        #cpulist = re.split(r"[,: ]+", cpu_line)
-        
-        #if len(cpulist) >= 8:
-            #idle = float(cpulist[7])
-            #msg.data = 100 - idle
-     #def main():
-    #node.create_timer(1, cb)
-    #rclpy.spin(node)
-       #pub.publish(msg)
-# CPU使用率を取得する
-    #cpu_usage = psutil.cpu_percent(interval=1)
-    #msg.data = cpu_usage
-    #pub.publish(msg)
-    #print(f"CPU使用率: {cpu_usage}%")
-
-#def main():
-    #node.create_timer(1, cb)
-    #rclpy.spin(node)
-
