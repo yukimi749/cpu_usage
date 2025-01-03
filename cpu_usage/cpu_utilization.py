8ff54cb861fbad7882c953340bdb72c1eb109945
Yukimi Miyahara <yukimi_pooh@au.com>
2025-01-03 16:16:38 +0900

    Change a node

--- cpu_usage/cpu_utilization.py
+++ cpu_usage/cpu_utilization.py
@@ -3,24 +3,16 @@ from rclpy.node import Node
 from std_msgs.msg import Float32
 import psutil
 
-class Cpu_utilization(Node):
-    def __init__(self):
-        super().__init__("cpu_utilization")
-        self.pub = self.create_publisher(Float32, "cpu", 10)
-        self.sub = self.create_subscription(Float32,"cpu",self.subscriber_callback,10)
+rclpy.init()
+node = Node("cpu_utilization")
+pub = node.create_publisher(Float32, "cpu", 10)
 
-        self.create_timer(1, self.cb)
-
-    def cb(self):
-        msg = Float32()
-        self.cpu_usage = psutil.cpu_percent(interval=1)
-        msg.data = self.cpu_usage
-        self.pub.publish(msg)
-        
-    def subscriber_callback(self, msg):
-        self.get_logger().info(f'cpu_usage: "{msg.data}"')
+def cb():
+    msg = Float32()
+    cpu_usage = psutil.cpu_percent(interval=1)
+    msg.data = cpu_usage
+    pub.publish(msg)
 
 def main():
-    rclpy.init()
-    node = Cpu_utilization()
+    node.create_timer(1, cb)
     rclpy.spin(node)
