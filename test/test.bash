cf999f27a35b86b347466f9b243967dbbd9f7362
Yukimi Miyahara <yukimi_pooh@au.com>
2025-01-03 16:19:24 +0900

    Change test

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,9 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch cpu_usage utilization.launch.py > /tmp/cpu_usage.log
+
+timeout 10 ros2 launch cpu_usage utilization.launch.py > /tmp/cpu_usage.log 
 
 cat /tmp/cpu_usage.log | grep -E '(100|[1-9][0-9]?)'
+
 
